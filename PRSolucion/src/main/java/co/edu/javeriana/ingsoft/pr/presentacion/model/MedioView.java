1ecd97f3fc73a2619be1373f64e4b90a31be54fe
Angela Maria Ospina A <am.ospina@javeriana.edu.co>
2021-10-27 13:55:21 -0500

    Agregamos clase medioView en la vista

---
+++ PRSolucion/src/main/java/co/edu/javeriana/ingsoft/pr/presentacion/model/MedioView.java
@@ -0,0 +1,16 @@
+package co.edu.javeriana.ingsoft.pr.presentacion.model;
+
+import co.edu.javeriana.ingsoft.pr.core.modelo.Medio;
+import co.edu.javeriana.ingsoft.pr.core.modelo.Periodista;
+import javafx.beans.property.SimpleStringProperty;
+
+public class MedioView {
+    private Medio medio;
+
+    public MedioView(Medio m)
+    {
+        this.medio = m;
+    }
+
+
+}
